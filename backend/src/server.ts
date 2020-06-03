d728ef963d8bc5c58c359a70e48625fc814f37d1
daniel-santos-7 <danielsantos7@outlook.com.br>
2020-06-03 11:35:59 -0300

    arquivos estáticos de items inseridos

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 
 const server = express();
 
 server.use(express.json());
 server.use(routes);
+server.use('/uploads',express.static(path.resolve(__dirname,'..','uploads')));
 
 export default server;
