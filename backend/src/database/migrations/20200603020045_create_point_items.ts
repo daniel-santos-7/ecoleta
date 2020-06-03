d02a1f8494629fc7f1e058b289b64489acf17fa4
daniel-santos-7 <danielsantos7@outlook.com.br>
2020-06-03 18:26:22 -0300

    erro corrigido ao declarar associações

--- backend/src/database/migrations/20200603020045_create_point_items.ts
+++ backend/src/database/migrations/20200603020045_create_point_items.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex): Promise<any> {
     return knex.schema.createTable('point_items', table=> {
         table.increments('id').primary();
         table.integer('point_id').notNullable().references('id').inTable('points');
-        table.integer('item_id').notNullable().references('id').inTable('item');
+        table.integer('item_id').notNullable().references('id').inTable('items');
     });
 }
 
