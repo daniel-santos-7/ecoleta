21ed37ea44926bed97408fde37bb9f74ad061eb5
daniel-santos-7 <danielsantos7@outlook.com.br>
2020-06-04 14:35:05 -0300

    página de cadastro de pontos criada

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles.css';
 import logo from '../../assets/logo.svg';
 import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
   return (
@@ -13,7 +14,7 @@ const Home: React.FC = () => {
         <main>
           <h1>Seu marketplace de coleta de resíduos.</h1>
           <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
-          <a href="/cadastro"><span><FiLogIn/></span><strong>Cadastre um ponto de coleta</strong></a>
+          <Link to="/create-point"><span><FiLogIn/></span><strong>Cadastre um ponto de coleta</strong></Link>
         </main>
       </div>
     </div>
